0ae33c792f8876a65379bcfe0390313f08dfb7aa
gajasri <gajasri@gmail.com>
2021-01-12 17:51:28 +0530

    header and footer added

---
+++ gitpro/common/footer.blade.php
@@ -0,0 +1,189 @@
+    <!-- footer start -->
+    <footer class="bg-light-white relative">
+        <div class="footer-style pt-80 pb-80">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-box mb-md-80">
+                            <div class="ft-logo mb-xl-20">
+                                <a href="index.html">
+                                    <img src="assets/images/logo/mg-logo2.png" class="img-fluid"
+                                    alt="img">
+                                </a>
+                            </div>
+                            <p class="text-custom-black mb-xl-20">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <ul class="custom contact-detail">
+                                <li>
+                                    <a href="#" class="text-custom-black fs-14">
+                                        <i class="fas fa-phone-alt text-custom-pink"></i>
+                                        <span class="ml-2">(+91) 9791346334 </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-custom-black fs-14">
+                                        <i class="fas fa-map-marker-alt text-custom-pink"></i>
+                                        <span class="ml-2">#256  Mettu Kunnur, Salai Road, TN, India -  632510 </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-custom-black fs-14">
+                                        <i class="fas fa-envelope text-custom-pink"></i>
+                                        <span class="ml-2">info@eravatheeswarar.com</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-box mb-md-80">
+                            <div class="ft-head">
+                                <h5 class="fs-23 f-700 mb-xl-20">Quick Links</h5>
+                            </div>
+                            <ul class="custom links">
+                                
+								<li class="mb-10">
+                                    <a href="#" class="text-custom-black">About Us</a>
+                                </li>
+                                <li class="mb-10">
+                                    <a href="#" class="text-custom-black">Contact Us</a>
+                                </li>
+                                <li class="mb-10">
+                                    <a href="#" class="text-custom-black">Meditation</a>
+                                </li>
+                                <li class="mb-10">
+                                    <a href="#" class="text-custom-black">7 Time's KAILASH </a>
+                                </li>
+                                <li class="mb-10">
+                                    <a href="#" class="text-custom-black">Privacy Policy</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-custom-black">Terms</a>
+                                </li>
+								
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-box mb-xs-80">
+                            <div class="ft-head">
+                                <h5 class="fs-23 f-700 mb-xl-20">Arti Times</h5>
+                            </div>
+                            <div class="temple-events">
+                                <div class="event-box mb-10">
+                                    <h6 class="mb-1 fs-18"><a href="#">Ganga Arti</a></h6>
+                                    <cite class="text-custom-pink fs-12">February 25 @ 10:00 am - 12:00 pm</cite>
+                                </div>
+                                <div class="event-box mb-10">
+                                    <h6 class="mb-1 fs-18"><a href="#">Holy Gathering</a></h6>
+                                    <cite class="text-custom-pink fs-12">February 25 @ 10:00 am - 12:00 pm</cite>
+                                </div>
+                                <div class="event-box mb-10">
+                                    <h6 class="mb-1 fs-18"><a href="#">Durga Puja</a></h6>
+                                    <cite class="text-custom-pink fs-12">February 25 @ 10:00 am - 12:00 pm</cite>
+                                </div>
+                                <div class="event-box">
+                                    <h6 class="mb-1 fs-18"><a href="#">Shivratri</a></h6>
+                                    <cite class="text-custom-pink fs-12">February 25 @ 10:00 am - 12:00 pm</cite>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-box">
+                            <div class="ft-head">
+                                <h5 class="fs-23 f-700 mb-xl-20">Follow Us</h5>
+                            </div>
+                            <ul class="custom instagram">
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/images/insta/insta-6.jpg" class="image-fit" alt="instagram">
+                                    </a>
+                                    <div class="insta-icon">
+                                        <a href="#">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/images/insta/insta-5.jpg" class="image-fit" alt="instagram">
+                                    </a>
+                                    <div class="insta-icon">
+                                        <a href="#">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/images/insta/insta-4.jpg" class="image-fit" alt="instagram">
+                                    </a>
+                                    <div class="insta-icon">
+                                        <a href="#">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/images/insta/insta-1.jpg" class="image-fit" alt="instagram">
+                                    </a>
+                                    <div class="insta-icon">
+                                        <a href="#">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/images/insta/insta-2.jpg" class="image-fit" alt="instagram">
+                                    </a>
+                                    <div class="insta-icon">
+                                        <a href="#">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/images/insta/insta-3.jpg" class="image-fit" alt="instagram">
+                                    </a>
+                                    <div class="insta-icon">
+                                        <a href="#">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer pb-35">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+                        <div class="hr-2 mb-35 bg-black opacity-1"></div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 text-center text-lg-left mb-md-10">
+                        <ul class="social-icons footer-social">
+                            <li> <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-google-plus-g"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6 col-md-12 text-center text-lg-right">© Eravatheeswarar Temple 2020 Allright Reserved</div>
+                </div>
+            </div>
+            <a href="#" class="btn scroll-btn f-right flex-center z-25 opacity-0"> <i class="fas fa-arrow-up"></i>
+            </a>
+        </div>
+    </footer>
+	
---
+++ gitpro/common/header.blade.php
@@ -0,0 +1,263 @@
+   <!-- Header start -->
+    <div class="topbar">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-5 col-sm-12 col-4">
+                    <div class="top-link left-side">
+                        <ul class="custom">
+                            <li>
+                                <a href="tel:"><i class="fas fa-phone-alt "></i> (+91) 9791346334</a>
+                            </li>
+                            <li>
+                                <a href="mailto:info@domain.com"><i class="fas fa-envelope "></i> info@eravatheeswarar.com</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fas fa-map-marker-alt "></i> Sholingar, TN.(India)</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-12 col-4">
+                    <div class="top-link center">
+                        <ul class="custom">
+                            <li>
+                                <a href="#"><i class="fab fa-facebook-f "></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fab fa-twitter "></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fab fa-linkedin-in "></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fab fa-google-plus-g "></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-sm-12 col-4">
+                    <div class="top-link right-side">
+                        <ul class="custom">
+                            <li>
+                                <a href="#"><i class="fas fa-map-marker-alt "></i>Temple Location</a>
+                            </li>
+							
+							<li>
+                                <a href="#"><i class="fas fa-heart "></i> Meditation & Instruction </a>
+                            </li>
+							
+                           <!-- <li>
+                                <a href="login.html"><i class="fas fa-lock "></i> Login & Signup</a>
+                            </li>
+                            <li>
+                                <a href="cart.html"><i class="fas fa-heart "></i> Wishlist</a>
+                            </li> -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <header class="type4 transition-5 relative mg-header mg-header-dark">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-md-2 col-4">
+                    <div class="logo2">
+                        <a href="index.html">
+                            <img src="assets/images/logo/mg-logo.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-8 col-4 text-right ">
+                    <span class="toggle-category">
+                        <i class="fas fa-chevron-down"></i>
+                      </span>
+                    <ul class="nav nav-links1 list-type2 justify-content-center">
+                        <li class="mg-nav-item mg-nav-item-has-children nav-link six-link active">
+                            <a href="#">HOME</a>
+                           <!-- <ul class="sub-menu">
+                                <li class="mg-nav-item nav-link"> <a href="index.html">Homepage 1</a></li>
+                                <li class="mg-nav-item nav-link"> <a href="homepage-2.html">Homepage 2</a></li>
+                                <li class="mg-nav-item nav-link"> <a href="homepage-3.html">Homepage 3</a></li>
+                                <li class="mg-nav-item nav-link"> <a href="homepage-4.html">Homepage 4</a></li>
+                                <li class="mg-nav-item nav-link"> <a href="homepage-5.html">Homepage 5</a></li>
+                                <li class="mg-nav-item nav-link"> <a href="homepage-6.html">Homepage 6</a></li>
+                            </ul> -->                        </li>
+                       <!-- <li class="nav-link"><a href="communities.html">COMMUNITIES</a></li>
+                        <li class="nav-link"><a href="sermons.html">SERMONS</a></li> -->
+                        <li class="nav-link"><a href="events.html">EVENTS</a></li>
+                        <li class="nav-link"><a href="blog.html">ABOUT US</a></li>
+                        <li class="nav-link"><a href="services.html">7  <span style="font-size:10px;padding:0px 5px;">Time's</span> KAILASH</a></li>
+
+                       <!-- <li class="mg-nav-item mg-nav-item-has-children nav-link">
+                            <a href="#">SHOP</a>
+                            <ul class="sub-menu">
+                                <li class="mg-nav-item nav-link"> <a href="shop-listing.html">Shoping-list-1</a></li>
+                                <li class="mg-nav-item nav-link"> <a href="shop-listing-2.html">Shoping-list 2</a></li>
+                                <li class="mg-nav-item nav-link"> <a href="product-details.html">product-details</a></li>
+                                <li class="mg-nav-item nav-link"> <a href="cart.html">cart</a></li>
+
+                            </ul>
+                        </li> -->
+
+
+                        <!-- mega menu page-->
+                        <li class="mg-nav-item mg-nav-item-has-children has-mega-menu mg-mega-menu--4-col-2 nav-link">
+                            <a href="#">TEMPLE INFO</a>
+                            <ul class="sub-menu">
+                                <li class="mg-mega-menu-wrapper clearfix">
+                                   <ul class="mg-mega-menu">
+                                        <li class="mg-nav-item mg-mega-menu-title pl-0"> Info
+                                            <li class="mg-nav-item nav-link"> <a href="blog-detail.html">Photos</a> </li>
+                                            <li class="mg-nav-item nav-link"> <a href="blog-detail-sidebar-left.html">Videos</a> </li>
+											<li class="mg-nav-item nav-link"> <a href="our-partner.html">Our Partner</a> </li>
+                                            <li class="mg-nav-item nav-link"> <a href="our-member.html">Our Member</a> </li>
+                                            <li class="mg-nav-item nav-link"> <a href="contact-1.html">Contact Us </a> </li>
+                                        
+                                        </li>
+
+                                    </ul>
+                                    <ul class="mg-mega-menu">
+                                        <li class="mg-nav-item mg-mega-menu-title v-hidden"> God
+                                            <li class="mg-nav-item nav-link"> <a href="our-partner.html">1008 SHIVA</a> </li>
+											
+											<li class="mg-nav-item nav-link"> <a href="our-partner.html">ADHI PHARA SAKTHi</a> </li>
+											
+											<li class="mg-nav-item nav-link"> <a href="our-partner.html">VINAYAGAR</a> </li>
+											<li class="mg-nav-item nav-link"> <a href="our-partner.html">ARUMUGAN</a> </li>
+                                            
+											
+                                        </li>
+                                    </ul>
+									
+                                    <ul class="mg-mega-menu">
+                                        <li class="mg-nav-item mg-mega-menu-title v-hidden"> God
+                                            <li class="mg-nav-item nav-link"> <a href="our-partner.html">ANCHANEYAR</a> </li>
+											<li class="mg-nav-item nav-link"> <a href="contact-1.html">SANISWARAN</a> </li>
+                                                                                       
+										    <li class="mg-nav-item nav-link"> <a href="our-member.html">KETU</a> </li>
+                                            <li class="mg-nav-item nav-link"> <a href="contact-1.html">RAHU</a> </li>
+                                            
+											
+                                        </li>
+                                    </ul>
+                                  
+								   </li>
+                            </ul>
+                            </li>
+
+                            <!-- mega menu page-->
+                    </ul>
+                </div>
+                <!-- <div class="col-md-2 col-4 text-center">
+                    <a href="#" class="search-icon mr-150 mr-md-75 mr-xs-55" data-toggle="modal" data-target="#searchModal"> <i class="fas fa-search fs-18 white mt-5"></i>
+                    </a>
+                </div> -->
+            </div>
+        </div>
+        <a href="donate.html" class="db-block ml-35 ml-xs-25">
+            Plan a Visit
+        </a>
+        <a href="#" class="menu-bars db-none right-menu ml-35 ml-xs-25">
+            <div class="bars transform-center">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </a>
+        <!--RIGHT MENU-->
+        <div class="col-md-2 col-2 text-right">
+            <div class="icon-links d-flex align-items-center justify-content-end">
+                <div class="bg-menu-overlay transition-5 opacity-8"></div>
+                <div class="fx-menu-wrapper bg-yellow-op-9 transition-5 d-flex justify-content-between flex-column">
+                    <div class="fx-menu-header d-flex justify-content-between">
+                        <p class="rbt-slab fs-13 f-700">Eravatheeswarar Temple</p>
+                        <div class="close-menu">
+                            <a href="#" class=""> <span></span>
+                                <span></span>
+                            </a>
+                        </div>
+                    </div>
+                    <a href="donate.html" class="btn btn-black mt-30 mb-35 mb-xs-20">Plan a Visit</a>
+                    <div class="fx-menu-content mb-15">
+                        <ul class="fx-menu-links rbt-slab">
+                            <li> <a href="#" >Home</a>
+                            </li>
+                        </ul>
+                        <ul class="fx-menu-links rbt-slab">
+                            <li> <a href="communities.html">7 <span style="font-size:10px;padding:0px 5px;">Time's</span>  Kailash</a>
+                            </li>
+                        </ul>
+                        <ul class="fx-menu-links rbt-slab">
+                            <li> <a href="events.html">Events</a>
+                            </li>
+                        </ul>
+                        <ul class="fx-menu-links rbt-slab">
+                            <li> <a href="#" class="has-sub">Temple</a>
+                                <ul class="submenu">
+                                    <li><a href="#" class="has-sub">Info</a>
+                                        <ul class="submenu menu-submenu-text">
+                                                         
+                                            <li class="mg-nav-item nav-link"> <a href="blog-detail.html">Photos</a> </li>
+                                            <li class="mg-nav-item nav-link"> <a href="blog-detail-sidebar-left.html">Videos</a> </li>
+											<li class="mg-nav-item nav-link"> <a href="our-partner.html">Our Partner</a> </li>
+                                            <li class="mg-nav-item nav-link"> <a href="our-member.html">Our Member</a> </li>
+                                            <li class="mg-nav-item nav-link"> <a href="contact-1.html">Contact Us </a> </li>
+                                        
+										    <li><a href="about-us.html">About Us</a>
+                                            </li>
+                                            </ul>
+                                    </li>
+                                    <li class="text-bold"><a href="#" class="has-sub">God</a>
+                                        <ul class="submenu menu-submenu-text">
+                                            <li class="mg-nav-item nav-link"> <a href="our-partner.html">1008 SHIVA</a> </li>
+											
+											<li class="mg-nav-item nav-link"> <a href="our-partner.html">ADHI PHARA SAKTHi</a> </li>
+											
+											<li class="mg-nav-item nav-link"> <a href="our-partner.html">VINAYAGAR</a> </li>
+											<li class="mg-nav-item nav-link"> <a href="our-partner.html">ARUMUGAN</a> </li>
+                                           
+										    <li class="mg-nav-item nav-link"> <a href="our-partner.html">ANCHANEYAR</a> </li>
+											<li class="mg-nav-item nav-link"> <a href="contact-1.html">SANISWARAN</a> </li>
+                                                                                       
+										    <li class="mg-nav-item nav-link"> <a href="our-member.html">KETU</a> </li>
+                                            <li class="mg-nav-item nav-link"> <a href="contact-1.html">RAHU</a> </li>
+                                           
+										   </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="fx-menu-footer">
+                        <ul class="mg-header social-icons menu-social black">
+                            <li> <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-google-plus-g"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- The search Modal start -->
+    <div class="search-popup modal" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <form action="#">
+                        <div class="form-group relative">
+                            <input type="text" class="form-control input-search" id="search" placeholder="Search here..."> <i class="fas fa-search transform-v-center"></i>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div> <i class="fas fa-times close-search-modal" data-dismiss="modal"></i>
+    </div>
+    <!-- The search Modal end -->
+    <!-- Header end -->
